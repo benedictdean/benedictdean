ae8e6b13aefed619f19a7f10dde54e825c1e226c
Ben <121672169+benedictdean@users.noreply.github.com>
2024-06-11 21:51:46 +0800

    1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var navbarLinks = document.getElementById("navbar-links");
+    if (navbarLinks.style.display === "block") {
+        navbarLinks.style.display = "none";
+    } else {
+        navbarLinks.style.display = "block";
+    }
+}
